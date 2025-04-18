121024daa51dc432c8a2d74549390da10b00b143
Mahar Bensyafri Taher <147983941+BenF10z@users.noreply.github.com>
2025-04-18 23:35:51 +0700

    Second commit

--- db.py
+++ db.py
@@ -1,1 +1,14 @@
-print("Hello World")
+import pymongo
+from dotenv import load_dotenv
+
+load_dotenv()
+
+myclient = pymongo.MongoClient("mongodb+srv://{DB_USERNAME}:{DB_PASSWORD}@maincluster.d2oiew4.mongodb.net/?retryWrites=true&w=majority&appName={CLUSTER_NAME}")
+
+mydb = myclient["horse_database"]
+mycol = mydb["horses"]
+
+mydict = { "name": "Tokai Teio", "wins": 8, "sex": "horse", "track": "Turf" }
+
+x = mycol.insert_one(mydict)
+print(x.inserted_id)
